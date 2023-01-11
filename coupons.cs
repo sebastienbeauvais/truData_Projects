2ca7954dff60e369379eba2ec88f3f47f0616236
Sebastien Beauvais <sbeauvais7@gmail.com>
2023-01-11 13:41:29 -0800

    fixed type error

--- coupons.cs
+++ coupons.cs
@@ -1,64 +1,39 @@
 using System;
 
-# class to apply discount 
-class Coupon{
-  public static void coupon(string[] args) {
-    Hashtable hashTable_coupons = new Hashtable();
-    hashTable_coupons.add("10% off", .1);
-    hashTable_coupons.add("BOGO", 1);
-
-    foreach (DictionaryEntry, dictionaryEntry in hashTable_coupons){
-      Console.WriteLine("{0} -> {1}", dictionaryEntry.Key, dictionaryEntry.Value);
+namespace ArchitectureProgram
+{
+    public class Coupon{
+        // Maybe use a hashtable with pre-defined coupons??
+        public string? Name;
+        public float Discount;
     }
-  }
-}
 
-# a class that stores key value pairs of an item and their price
-class Items{
-  public static void items(){
-    Hashtable hashTable_items = new Hashtable();
-    hashTable_items.add("Milk", 4.99);
-    hashtable_items.add("Eggs", 1.99);
-    hashTable_items.add("Bread", 3.99);
-    hashTable_items.add("Butter", 0.99);
-
-    foreach (DictionaryEntry, dictionaryEntry in hashTable_items){
-      Console.WriteLine("{0} -> {1}", dictionaryEntry.Key, dictionaryEntry.Value);
+    public class Item{
+        public string? Name;
+        public float Price;
+        public int Quantity;
     }
-  }
-}
 
-# class for cart total
-class Basket{
-  public static void basket(string[] args){
-    Console.WriteLine("Please specify which items you would like to purchase: ");
-    list cart = Console.ReadLine();
-    
-    # rough code
-    if (hashTable_items.ContainsKey({user input}))
-      Console.WriteLine("Item added to basket.");
-      # add item to Checkout
-      # next item check
-    else:
-      Console.WriteLine("Sorry that item doesn't exists.");
+    public class Cart{
+        
+    }
 
-    Console.WriteLine("Would you like to enter a coupon? (Y/N): ");
+    class Program{
+        public static void Main(string[] args){
+            Item item = new Item();
+            item.Name = "Milk";
+            item.Price = 3.99f;
+            item.Quantity = 2;
 
-    Console.WriteLine("Coupon Name: ");
-    string couponCode = Console.ReadLine()
-    if (hashTable_coupons.ContainsKet({couponCode}))
-      Console.WriteLine("Successfully added coupon.");
-      # update price
-    else
-      Console.WriteLine("Coupon does not exists.");
-  }
-}
+            Coupon coupon = new Coupon();
+            coupon.Name = "10% off cart total";
+            coupon.Discount = 0.1f;
 
-# class for final pricing
-class Checkout{
-  public static void Main(string[] args){
-    basket();
+            Console.WriteLine("Your cart consists of " + item.Name + " the price is " + item.Price + " and quantity ("+ item.Quantity +")");
+            //Console.WriteLine("Your Current Total: " + (item.Price*item.Quantity));
+            //Console.WriteLine("We will apply a coupon to your basket: " + coupon.Name);
+            //Console.WrtieLine("Your new cart total is: " + (item.Price*item.Quantity)*(1-coupon.Discount));
 
-    Console.WriteLine("Your final price after applying coupons is: " + discountPrice);
-  }
+        }
+    }
 }
