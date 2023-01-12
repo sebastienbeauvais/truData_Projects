57a7e440976300f00612af92693e01b823f98d18
Sebastien Beauvias <sbeauvais7@gmail.com>
2023-01-12 15:16:26 -0800

    added a rough hashtable

--- coupons.cs
+++ coupons.cs
@@ -1,39 +1,59 @@
-﻿using System;
+﻿using System.Collections;
 
 namespace ArchitectureProgram
 {
     public class Coupon{
         // Maybe use a hashtable with pre-defined coupons??
-        public string Name;
+        public string? Name;
         public float Discount;
     }
 
     public class Item{
-        public string Name;
-        public float Price;
-        public int Quantity;
-    }
 
-    public class Cart{
-        
     }
 
     class Program{
-        public void Main(string[] args){
-            Item item = new Item();
-            item.Name = "Milk";
-            item.Price = 3.99;
-            item.Quantity = 2;
+        public static void Main(string[] args){
+
+            Hashtable itemTable = new Hashtable();
+            itemTable.Add("Milk", 3.99f);
+            itemTable.Add("Eggs", 5.99f);
+            itemTable.Add("Bread", 2.99f);
+            itemTable.Add("Butter", 1.99f);
+
+            Hashtable couponTable = new Hashtable();
+            couponTable.Add("10%", 0.1f);
+            couponTable.Add("25%", 0.25f);
+            couponTable.Add("BOGO", 0.5f);
+            
 
             Coupon coupon = new Coupon();
             coupon.Name = "10% off cart total";
-            coupon.Discount = .1;
+            coupon.Discount = 0.1f;
 
-            //Console.WriteLine("Your cart consists of " + item.Name + " the price is " + item.Price + " and quantity ("+ item.Quantity +")");
-            //Console.WriteLine("Your Current Total: " + (item.Price*item.Quantity));
-            //Console.WriteLine("We will apply a coupon to your basket: " + coupon.Name);
-            //Console.WrtieLine("Your new cart total is: " + (item.Price*item.Quantity)*(1-coupon.Discount));
+            // Printing hashtable of items
+            foreach(string key in itemTable.Keys){
+                Console.WriteLine(String.Format("{0}: {1}", key, itemTable[key]));
+            }
+            
+            // Getting customer shopping list
+            List<string> shoppingList = new List<string>();
 
+            Console.WriteLine("Enter your shopping list: ");
+            string input = Console.ReadLine();
+            shoppingList.Add(input);
+            
+            
+            while (input != ""){
+                Console.WriteLine("Please enter another item: ");
+                input = Console.ReadLine();
+                shoppingList.Add(input);
+            }
+            if (input == ""){
+                foreach(string key in shoppingList){
+                    Console.WriteLine(key + " was added to the shopping list.");
+                }
+            }            
         }
     }
 }
